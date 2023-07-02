470c2e8e55544346bcbfd4622bc8586d9a365946
Mitch McNanna <mmcnanna@des81.fnal.gov>
2023-07-01 23:05:08 -0500

    Fixed mkdir calls

--- simulations/farm_percentages.sh
+++ simulations/farm_percentages.sh
@@ -2,13 +2,13 @@
 
 queue='vanilla'
 njobs=20
-hpxdir=/data/des81.b/data/y6a2/gold/2.0/healpix
-outdir=skim_y6_gold_2_0
+outdir=detection_percentages
 
 mkdir -p $outdir/log
 
 for d in $(seq 500 500 2000)
 do
+        mkdir -p $outdir/$d
 	for m in $(seq -2.5 -0.5 -14.0)
 	do
 		for a in $(seq 1.4 0.2 3.6)
